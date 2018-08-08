bc90593e43cdf9c89d2f31dfa978620f20342221
Aaron Thomsen <aarontthomsen@gmail.com>
2018-08-08 00:37:01 -0700

    Changed app.js

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ let app = express();
 
 let port = 4200;
 
-//app.use(express.static(__dirname + 'public_assets'));
-
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public_assets/home.html');
 });
