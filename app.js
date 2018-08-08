a3a02f9d5bd6ecb157715da6ebfe029f8741cf4c
Aaron Thomsen <aarontthomsen@gmail.com>
2018-08-08 00:39:52 -0700

    Configure for heroku!

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 let app = express();
 
-let port = 4200;
+let port = process.env.PORT || 4200;
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public_assets/home.html');
 });
 
-app.listen(port);
+app.listen(port, () => console.log(`Now listening on port ${port}`));
